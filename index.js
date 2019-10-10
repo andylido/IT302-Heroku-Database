ce28023f6d6c09cb483ac73cb2c4679fdb544e73
Andy Li <ali37@uic.edu>
2019-10-10 19:41:40 +0000

    Update Files

--- index.js
+++ index.js
@@ -31,16 +31,6 @@ express()
   .get('/times', (req, res) => res.send(showTimes()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
-
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 showTimes = () => {
   var result = ''
   const times = process.env.TIMES || 5
