79858e1e9a6db5c57c325288bf221b357a2e544a
Andy Li <ali37@uic.edu>
2019-11-19 06:56:56 +0000

    Updated endpoint

--- index.js
+++ index.js
@@ -9,24 +9,29 @@ const pool = new Pool({
   ssl: true
 });
 
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: "postgres://wwhmiddhrxhbrv:77b45dd41ea9cec7c088f4b3f0b956de82c35a0a839ad2df982a66ac9e4515aa@ec2-184-72-221-140.compute-1.amazonaws.com:5432/dct33ivp1i8p9s",
+  ssl: true,
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-.get('/db', async (req, res) => {
+.get('/employee', async (req, res) => {
     try {
       const client = await pool.connect()
-      const result = await client.query("SELECT * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'employee'");
+      const result = await client.query('SELECT * FROM employee;');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
+      res.render('pages/employee', results );
       client.release();
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
     }
   })
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
