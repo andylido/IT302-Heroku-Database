6bd450daa7d663ce2526cc69214dd6af91be5afb
Andy Li <ali37@uic.edu>
2019-11-20 18:31:59 +0000

    Updated employee endpoint

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ express()
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM employee;');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      console.log(results);
+      res.send(results);
+      //res.render('pages/db', results );
       client.release();
     } catch (err) {
       console.error(err);
