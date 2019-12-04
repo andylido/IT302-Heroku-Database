93d318af58d34cb3a34f72eaa5464d91328b95a4
Andy Li <ali37@uic.edu>
2019-12-04 16:50:47 +0000

    SQL commands update

--- create_tables.sql
+++ create_tables.sql
@@ -29,23 +29,23 @@ CREATE TABLE employee (
 
 CREATE TABLE account (
     account_id INT PRIMARY KEY,
-    customer_id INT REFERENCES customer(customer_id) NOT NULL, 
-    employee_id INT REFERENCES employee(employee_id) NOT NULL,
-    type VARCHAR(50) NOT NULL,
-    name VARCHAR(50) NOT NULL,
+    customer_id INT  NOT NULL, 
+    employee_id INT  NOT NULL,
+    account_type VARCHAR(50) NOT NULL,
+    account_name VARCHAR(50) NOT NULL,
     account_creation_date TIMESTAMP
 );
 
 CREATE TABLE position (
     position_id INT PRIMARY KEY,
-    asset_id INT REFERENCES asset(asset_id),
-    position_shares FLOAT(3)
+    asset_id INT,
+    position_shares INT
 );
 
 CREATE TABLE transaction (
     transaction_id int PRIMARY KEY,
-    account_id INT REFERENCES account(account_id),
-    asset_id INT REFERENCES asset(asset_id),
+    account_id INT,
+    security_id INT,
     transaction_type VARCHAR(1),
     transaction_shares FLOAT(3),
     transaction_price FLOAT(3),
@@ -53,11 +53,10 @@ CREATE TABLE transaction (
     transaction_status VARCHAR(1)
 );
 
-CREATE TABLE asset (
-    asset_id INT PRIMARY KEY,
-    asset_symbol VARCHAR(10) NOT NULL,
-    asset_name VARCHAR(50),
-    asset_type VARCHAR(3),
-    asset_exchange VARCHAR(10)
+CREATE TABLE security (
+    secutiry_id INT PRIMARY KEY,
+    secutity_symbol VARCHAR(10) NOT NULL,
+    security_name VARCHAR(50),
+    security_sector VARCHAR(50)
 );
 
